34d1de908279f927b7013dad3d8e09c42516ba3a
pataanggs <ryukikun4@gmail.com>
2024-11-06 13:30:48 +0700

    feat: delete hypertext

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { LuImport } from 'react-icons/lu';
 import pataangg from '../assets/pataangg.jpg';
-import HyperText from '../components/HyperText'; // Ensure this path is correct
 
 const Hero = () => {
   return (
     <section>
       <div className='flex flex-col items-center justify-center'>
-        <HyperText text="FATHAN" className="font-semibold leading-none text-[12vw]" />
-        <HyperText text="KARTAGAMA" className="font-semibold leading-none text-[12vw]" />
+        <h1 className='mt-16 overflow-hidden text-[12vw] font-semibold uppercase leading-none'>
+          Fathan <br/> Kartagama 
+        </h1>
         <div className='mt-8'>
           <a href='/CV Fathan Andi.pdf' target='_blank' rel='noopener noreferrer' download className='flex items-center rounded-xl bg-lime-300 p-2 px-3 font-sans font-medium text-black hover:bg-lime-400'>
             <span>Download My CV</span>
